d2d37c01c2920012d94f48f862a70c5a3324a65d
Umamah Hussain <152015569+umamahhussain@users.noreply.github.com>
2025-04-20 00:52:35 +0500

    Created Pages & Routes

--- Front-end/real-estate/src/App.jsx
+++ Front-end/real-estate/src/App.jsx
@@ -1,13 +1,24 @@
-import { useState } from 'react'
-import './App.css'
+import { useState } from "react";
+import { BrowserRouter, Routes, Routes } from "react-router-dom";
+import "./App.css";
+import Signup from "./pages/Signup";
+import Signin from "./pages/Signin";
+import Home from "./pages/Home";
+import Profile from "./pages/Profile";
+import About from "./pages/About";
 
 function App() {
-
-  return (
-    <>
-      
-    </>
-  )
+	return (
+		<BrowserRouter>
+			<Routes>
+        < Route path='/' element={<Home/>} />
+        < Route path='/sign-in' element={<Signin/>} />
+        < Route path='/sign-up' element={<Signup/>} />
+        < Route path='/profile' element={<Profile/>} />
+        < Route path='/about' element={<About/>} />
+      </Routes>
+		</BrowserRouter>
+	);
 }
 
-export default App
+export default App;
