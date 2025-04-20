ba2fbdcfc5fe482c6d41b661eb2cbf383fb4357d
Umamah Hussain <152015569+umamahhussain@users.noreply.github.com>
2025-04-20 23:35:03 +0500

    redux user integration

--- Front-end/real-estate/src/pages/Signin.jsx
+++ Front-end/real-estate/src/pages/Signin.jsx
@@ -1,12 +1,16 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import { useNavigate } from "react-router-dom";
+import { useDispatch, useSelector } from 'react-redux'
+import {signInStart, signInSuccess, signInFailure} from '../redux/user/userSlice'
+
 
 export default function Signin() {
 	const [formData, setFormData] = useState({});
-	const [error, setError] = useState(null);
-	const [loading, setLoading] = useState(false);
+	const {loading, error} = useSelector((state) => state.user);
 	const navigate = useNavigate();
+  const dispatch = useDispatch();
+
 
 	const handleChange = (e) => {
 		setFormData({
@@ -17,7 +21,7 @@ export default function Signin() {
 
 	const handleSubmit = async (e) => {
 		e.preventDefault();
-		setLoading(true);
+		dispatch(signInStart())
 
 		const res = await fetch("/api/auth/signin", {
 			method: "POST",
@@ -29,12 +33,10 @@ export default function Signin() {
 		const data = await res.json();
 
 		if (data.success === false) {
-			setError(data.message);
-			setLoading(false);
+			dispatch(signInFailure(data.message))
 			return;
 		}
-		setLoading(false);
-		setError(null);
+		dispatch(signInSuccess(data))
 		navigate("/");
 		console.log(data);
 	};
