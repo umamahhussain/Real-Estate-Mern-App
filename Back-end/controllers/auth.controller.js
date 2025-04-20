87257011909c87363cd4ca2f8dd19a1499c4fbc2
Umamah Hussain <152015569+umamahhussain@users.noreply.github.com>
2025-04-20 22:11:12 +0500

    signin API route

--- Back-end/controllers/auth.controller.js
+++ Back-end/controllers/auth.controller.js
@@ -1,5 +1,8 @@
 import User from '../models/user.model.js'
 import bcryptjs from 'bcryptjs'
+import { errorHandler } from '../utils/error.js';
+import jwt from 'jsonwebtoken'
+
 
 export const signup = async ( req, res, next) => {
 
@@ -19,5 +22,30 @@ export const signup = async ( req, res, next) => {
     catch (error) {
         next(error);
     }
-
 }
+
+
+export const signin = async (req, res, next) => {
+	const {  username, password } = req.body;
+
+	try {
+
+		const validUser = await User.findOne({ username });
+        if( !validUser ){
+            return next(errorHandler( 404, 'User not found!' ));
+        }
+
+        const validPassword = bcryptjs.compareSync( password, validUser.password )
+        if( !validPassword ){
+            return next(errorHandler(401, "Invalid Credentials!"));
+        }
+
+        const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET, { expiresIn: '1h' });
+        const { password: pass, ...userInfo } = validUser._doc
+        res.cookie( 'access_token', token, {httpOnly: true} ).status(200).json(userInfo);
+
+	} catch (error) 
+    {
+		next(error);
+	}
+};
