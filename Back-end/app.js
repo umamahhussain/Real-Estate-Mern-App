295840b9feb5d53c44aadc1208e1a25455a7e966
Umamah Hussain <152015569+umamahhussain@users.noreply.github.com>
2025-04-20 15:45:45 +0500

    signup routes

--- Back-end/app.js
+++ Back-end/app.js
@@ -1,7 +1,8 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
-import userRouter from './routes/user.route'
+import userRouter from './routes/user.route.js'
+import authRouter from './routes/auth.route.js'
 
 dotenv.config();
 
@@ -13,8 +14,11 @@ mongoose.connect(process.env.Mongo).then(() => {
 ;
 
 const app = express();
+app.use(express.json());
+
 app.listen(3000, () => {
 	console.log("Server is runninggg");
 });
 
 app.use( '/user', userRouter );
+app.use ( '/auth', authRouter );
