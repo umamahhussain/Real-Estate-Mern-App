de3c06b67d2afb5cb3b05a8d08a15bfec05afde7
Umamah Hussain <152015569+umamahhussain@users.noreply.github.com>
2025-04-26 17:21:18 +0500

    complete listing page

--- Front-end/real-estate/src/pages/CreateListing.jsx
+++ Front-end/real-estate/src/pages/CreateListing.jsx
@@ -269,7 +269,9 @@ export default function CreateListing() {
 							/>
 							<div className="flex flex-col items-start">
 								<p>Regular price</p>
-								<span className="text-xs">($ / month)</span>
+								{formData.type === "rent" && (
+									<span className="text-xs">($ / month)</span>
+								)}
 							</div>
 						</div>
 						<div className="flex items-center gap-2">
@@ -283,7 +285,9 @@ export default function CreateListing() {
 							/>
 							<div className="flex flex-col items-start">
 								<p>Discounted price</p>
-								<span className="text-xs">($ / month)</span>
+								{formData.type === "rent" && (
+									<span className="text-xs">($ / month)</span>
+								)}
 							</div>
 						</div>
 					</div>
