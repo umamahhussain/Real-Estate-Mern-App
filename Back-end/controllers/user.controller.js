890a2c46f31d1493123cc12ba501e4848fe35d2f
Umamah Hussain <152015569+umamahhussain@users.noreply.github.com>
2025-04-26 15:51:07 +0500

    listing functionality v1

--- Back-end/controllers/user.controller.js
+++ Back-end/controllers/user.controller.js
@@ -1,6 +1,7 @@
 import User from "../models/user.model.js";
 import bcryptjs from "bcryptjs";
 import { errorHandler } from "../utils/error.js";
+import Listing from "../models/listing.model.js";
 
 export const updateAvatar = async (req, res, next) => {
 	try {
@@ -64,3 +65,19 @@ export const updateUser = async (req, res, next) => {
 			next(error);
 		}
  };
+
+
+ export const getUserListings = async (req, res, next) => {
+		if (req.user.id === req.params.id) {
+			try {
+				const listings = await Listing.find({ userRef: req.params.id });
+				res.status(200).json(listings);
+			} catch (error) {
+				next(error);
+			}
+		} else {
+			return next(errorHandler(401, "You can only view your own listings!"));
+		}
+ };
+
+
--- Back-end/routes/user.route.js
+++ Back-end/routes/user.route.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { updateAvatar, updateUser, deleteUser } from "../controllers/user.controller.js";
+import { updateAvatar, updateUser, deleteUser, getUserListings } from "../controllers/user.controller.js";
 import { verifyToken } from "../utils/verifyToken.js";
 
 const router = express.Router();
@@ -7,5 +7,6 @@ const router = express.Router();
 router.put('/update-avatar', verifyToken ,updateAvatar);
 router.post("/update/:id", verifyToken, updateUser);
 router.delete("/delete/:id", verifyToken, deleteUser);
+router.get("/listings/:id", verifyToken, getUserListings);
 
 export default router;
