f8d050e3d2191f735b129bc8282a759575327021
Umamah Hussain <152015569+umamahhussain@users.noreply.github.com>
2025-04-26 17:26:24 +0500

    Land Lord Functionality

--- Back-end/routes/user.route.js
+++ Back-end/routes/user.route.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { updateAvatar, updateUser, deleteUser, getUserListings } from "../controllers/user.controller.js";
+import { updateAvatar, updateUser, deleteUser, getUserListings, getUser } from "../controllers/user.controller.js";
 import { verifyToken } from "../utils/verifyToken.js";
 
 const router = express.Router();
@@ -8,5 +8,6 @@ router.put('/update-avatar', verifyToken ,updateAvatar);
 router.post("/update/:id", verifyToken, updateUser);
 router.delete("/delete/:id", verifyToken, deleteUser);
 router.get("/listings/:id", verifyToken, getUserListings);
+router.get("/:id", verifyToken, getUser);
 
 export default router;
--- Front-end/real-estate/src/pages/Listing.jsx
+++ Front-end/real-estate/src/pages/Listing.jsx
@@ -2,6 +2,8 @@ import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { Swiper, SwiperSlide } from "swiper/react";
 import SwiperCore from "swiper";
+import { useSelector } from "react-redux";
+import Contact from "../components/Contact";
 import { Navigation } from "swiper/modules";
 import "swiper/css/bundle";
 import {
@@ -20,6 +22,8 @@ export default function Listing() {
 	const [loading, setLoading] = useState(false);
 	const [error, setError] = useState(false);
 	const [copied, setCopied] = useState(false);
+	const [contact, setContact] = useState(false); 
+	const { currentUser } = useSelector((state) => state.user);
 	const params = useParams();
 	
 	useEffect(() => {
@@ -131,6 +135,15 @@ export default function Listing() {
 								{listing.furnished ? "Furnished" : "Unfurnished"}
 							</li>
 						</ul>
+						{currentUser && listing.userRef !== currentUser._id && !contact && (
+							<button
+								onClick={() => setContact(true)}
+								className="bg-slate-700 text-white rounded-lg uppercase hover:opacity-95 p-3"
+							>
+								Contact landlord
+							</button>
+						)}
+						{contact && <Contact listing={listing} />}
 					</div>
 				</div>
 			)}
