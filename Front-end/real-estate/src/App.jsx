c9fef7ac896ce1b497c54c33b3883b1831a39b28
Umamah Hussain <152015569+umamahhussain@users.noreply.github.com>
2025-04-26 18:46:25 +0500

    Search v1

--- Front-end/real-estate/src/App.jsx
+++ Front-end/real-estate/src/App.jsx
@@ -6,11 +6,12 @@ import Signin from "./pages/Signin";
 import Home from "./pages/Home";
 import Profile from "./pages/Profile";
 import About from "./pages/About";
-import Header from "./components/header";
+import Header from "./components/Header";
 import PrivateRoute from "./components/PrivateRoute";
 import CreateListing from "./pages/CreateListing";
 import UpdateListing from "./pages/UpdateListing";
 import Listing from "./pages/Listing";
+import Search from "./pages/Search";
 
 function App() {
 	return (
@@ -27,6 +28,7 @@ function App() {
 				<Route path="/about" element={<About />} />
 				<Route path="/update-listing/:listingId" element={<UpdateListing />} />
 				<Route path="/listing/:listingId" element={<Listing />} />
+				<Route path="/search" element={<Search />} />
 			</Routes>
 		</BrowserRouter>
 	);
